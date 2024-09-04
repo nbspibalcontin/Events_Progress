dc8e02a8773afa4a65c33317fe7be02cedf93e49
ebenezer.v.alcontin <ebenezer.v.alcontin@MIS-DEV>
2024-09-04 16:51:39 +0800

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp4.Progress;
+using ConsoleApp4.Progress.Implementation;
+using ConsoleApp4.Progress.Interface;
+
+public class Program
+{
+    private static readonly IDownloader downloaderService = new DownloaderService();
+
+    public static void Main(string[] args)
+    {
+        // Define the callback method
+        ProgressCallback callback = progress => Console.WriteLine($"Download progress: {progress}%");
+
+        downloaderService.StartDownload(callback);
+    }
+}
---
+++ ConsoleApp4/Progress/Implementation/DownloaderService.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp4.Progress.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.Progress.Implementation
+{
+    public class DownloaderService : IDownloader
+    {
+        public void StartDownload(ProgressCallback progressCallback)
+        {
+            for (int i = 0; i <= 100; i += 10)
+            {
+                Thread.Sleep(500); // Simulate download work
+                progressCallback?.Invoke(i); // Invoke the callback to report progress
+            }
+        }
+    }
+}
